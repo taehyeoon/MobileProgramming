83ce514b8bfc485fc0c5eeb07d59798cddc7edfa
taehyeon <tedxpem@naver.com>
2023-05-14 21:23:57 +0900

    todolist 초기 구현
    메인화면 디자인
    투두리스트로 이동하는 버튼 한개 활성화

---
+++ Todolist/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.todolist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todolist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.apply {
+            plusTodolistBtn.setOnClickListener {
+                val i = Intent(this@MainActivity, TodolistActivity::class.java)
+                startActivity(i)
+            }
+        }
+
+    }
+
+
+}
